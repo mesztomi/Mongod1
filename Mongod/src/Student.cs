793af26879759317a7b36f791f0f65341d0f02ed
Mészáros Tamás <tomingher@hotmail.com>
2015-08-18 00:18:39 +0200

    HomeWork 2.1

---
+++ Mongod/src/Student.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mongod.src
+{
+    class Student
+    {
+        public ObjectId Id { get; set; }
+
+        public int student_id { get; set; }
+
+        public string type { get; set; }
+
+        public double score { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Id: {0}, StudenId: \"{1}\", type: {2}, Score: \"{3}\"", Id, student_id, type, score);
+        }
+    }
+}
